4c9d479e563b7d211a5708d90665c490a78c69d1
hckim96 <hyeoncheolkim96@gmail.com>
2023-06-23 13:35:49 +0900

    :wrench: QueryDsl 설정

--- src/main/java/com/tbfp/teamplannerbe/TeamPlannerBeApplication.java
+++ src/main/java/com/tbfp/teamplannerbe/TeamPlannerBeApplication.java
@@ -1,7 +1,12 @@
 package com.tbfp.teamplannerbe;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class TeamPlannerBeApplication {
---
+++ src/main/java/com/tbfp/teamplannerbe/domain/member/entity/Member.java
@@ -0,0 +1,33 @@
+package com.tbfp.teamplannerbe.domain.member.entity;
+
+import com.tbfp.teamplannerbe.domain.common.base.BaseTimeEntity;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+
+    @Column(length = 20)
+    private String loginId;
+
+    @Column(length = 20)
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private Boolean state;
+
+}

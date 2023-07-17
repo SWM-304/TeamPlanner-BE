d885638b1e3d69a19de01d97cc515a11144dbcee
hckim96 <hyeoncheolkim96@gmail.com>
2023-07-17 15:06:42 +0900

    :adhesive_bandage: sql 잘못 올라간 것 수정

--- src/main/resources/db/migration/V4__create_recruitment_like.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists recruitment_like;
-CREATE TABLE recruitment_like (
-                                  recruitment_like_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                  member_id BIGINT,
-                                  recruitment_id BIGINT,
-                                  FOREIGN KEY (member_id) REFERENCES member (member_id),
-                                  FOREIGN KEY (recruitment_id) REFERENCES recruitment (recruitment_id)
-);

d3f165741c8880c19340d72c66325bfe7d8a6e5e
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-09-07 12:29:51 +0900

    :bug: prod update

--- scripts/pull_docker.sh
+++ scripts/pull_docker.sh
@@ -3,8 +3,6 @@
 # Public 이미지 받기 trick :sudo docker logout public.ecr.aws
 sudo docker logout public.ecr.aws
 sleep 0.5
-sudo aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com
-sleep 0.5
 sudo docker rmi -f 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest
 sleep 0.5
 sudo docker pull 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest

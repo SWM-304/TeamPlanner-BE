99305f14b360fa994b8845c9e634e2b525e0cea2
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-09-07 22:28:28 +0900

    :sparkels: prod update

--- scripts/pull_docker.sh
+++ scripts/pull_docker.sh
@@ -5,6 +5,6 @@ sudo docker logout public.ecr.aws
 sleep 0.5
 sudo docker rmi -f 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest
 sleep 0.5
+sudo aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com
+sleep 0.5
 sudo docker pull 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest
-#sleep 0.5
-#sudo docker tag 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest pub-web:city-2.0

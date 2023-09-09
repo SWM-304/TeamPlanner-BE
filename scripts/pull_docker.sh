6cede8577663f23b02383619cd9ed633ea75e4ae
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-09-10 00:22:03 +0900

    :sparkels: prod update

--- scripts/pull_docker.sh
+++ scripts/pull_docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# 기능 : 도커이미지를 가져와서 태그설정
-# Public 이미지 받기 trick :sudo docker logout public.ecr.aws
-sudo docker logout public.ecr.aws
-sleep 0.5
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com
-sleep 5
+# 기능 : 도커 이미지를 가져와서 태그 설정
+# Public 이미지 받기 trick: sudo docker logout public.ecr.aws
+
+# Docker 로그인 토큰을 환경 변수에 저장
+DOCKER_LOGIN_TOKEN=$(aws ecr get-login-password --region ap-northeast-2)
+
+# Docker 로그인
+echo "$DOCKER_LOGIN_TOKEN" | docker login --username AWS --password-stdin 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com
+
+# 이미지 이름
 image_name="129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest"
 
 # 이미지가 존재하는지 확인
@@ -16,4 +20,5 @@ else
     echo "Docker 이미지 '$image_name'는 이미 존재하지 않습니다."
 fi
 
-docker pull 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest
+# Docker 이미지 가져오기
+docker pull "$image_name"

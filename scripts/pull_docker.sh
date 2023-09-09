d8c78e1a11c8a896c5aa6d6b521ef3126739eb29
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-09-10 00:30:31 +0900

    :sparkels: prod update

--- scripts/pull_docker.sh
+++ scripts/pull_docker.sh
@@ -6,7 +6,7 @@
 DOCKER_LOGIN_TOKEN=$(aws ecr get-login-password --region ap-northeast-2)
 
 # Docker 로그인
-echo "$DOCKER_LOGIN_TOKEN" | docker login --username AWS --password-stdin 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com
+echo "$DOCKER_LOGIN_TOKEN" | sudo docker login --username AWS --password-stdin 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com
 
 # 이미지 이름
 image_name="129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest"
@@ -21,4 +21,4 @@ else
 fi
 
 # Docker 이미지 가져오기
-docker pull "$image_name"
+sudo docker pull "$image_name"

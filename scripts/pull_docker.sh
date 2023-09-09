1475128e84d0a02b1c57e80ad2741df5b2ebc4fa
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-09-09 23:22:16 +0900

    :sparkels: prod update

--- scripts/pull_docker.sh
+++ scripts/pull_docker.sh
@@ -5,6 +5,16 @@ sudo docker logout public.ecr.aws
 sleep 0.5
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com
 sleep 5
-sudo docker rmi -f 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest
+image_name="129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest"
+
+# 이미지가 존재하는지 확인
+if docker images | awk '{print $1}' | grep -q "^$image_name\$"; then
+    # 이미지가 존재하는 경우 삭제
+    sudo docker rmi -f "$image_name"
+    echo "Docker 이미지 '$image_name'를 삭제했습니다."
+else
+    echo "Docker 이미지 '$image_name'는 이미 존재하지 않습니다."
+fi
+
 sleep 0.5
 sudo docker pull 129715120090.dkr.ecr.ap-northeast-2.amazonaws.com/teamplanner-backendserver:latest

46a892f1c6eec09d1a4db3fc6a0252b62cc21c27
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-10-11 11:55:52 +0900

    :bug: update prod

--- src/main/java/com/tbfp/teamplannerbe/domain/auth/oauth/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/tbfp/teamplannerbe/domain/auth/oauth/OAuth2LoginSuccessHandler.java
@@ -8,6 +8,7 @@ import com.tbfp.teamplannerbe.domain.common.exception.ApplicationErrorType;
 import com.tbfp.teamplannerbe.domain.common.exception.ApplicationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -24,6 +25,9 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtProvider jwtProvider;
     private final RefreshTokenService refreshTokenService;
 
+    @Value("${refer.host}")
+    private String referHost;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         log.info("OAuth2 Login 성공!");
@@ -33,34 +37,31 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         if(oAuth2Member.getMemberRole() == MemberRole.GUEST) {
             throw new ApplicationException(ApplicationErrorType.GUEST_USER);
         } else {
-            loginSuccess(response, oAuth2Member,getCurrentDomain(request)); // 로그인에 성공한 경우 access, refresh 토큰 생성
+            loginSuccess(response, oAuth2Member,referHost); // 로그인에 성공한 경우 access, refresh 토큰 생성
         }
     }
 
-    // 현재 도메인 정보를 가져오는 메서드
-    private String getCurrentDomain(HttpServletRequest request) {
-        String scheme = request.getScheme();
-        log.info("scheme = " + scheme);
-        String serverName = request.getServerName();
-        log.info("serverName = " + serverName);
-
-        log.info("request.getLocalPort() = " + request.getLocalPort());
-        log.info("request.getRemotePort() = " + request.getRemotePort());
-        log.info("request.getServerPort() = " + request.getServerPort());
-
-        String referer = request.getHeader("referer");
-        log.info("referer = " + referer);
-        String currentDomain="";
-
-        if (referer == null) {
-            // 소셜로그인 처음 성공하면 이럼 , 두번째 부터는 null 아님 프론트 주소 가져옴
-            // TODO: 나중에 수정, 임시로 localhost:3000
-            referer = "http://localhost:3000/";
-        }
-        currentDomain = referer;
-
-        return currentDomain;
-    }
+//    // 현재 도메인 정보를 가져오는 메서드
+//    private String getCurrentDomain(HttpServletRequest request) {
+//        String scheme = request.getScheme();
+////        log.info("scheme = " + scheme);
+////        String serverName = request.getServerName();
+////
+////
+////        String referer = request.getHeader("referer");
+////        log.info("referer = " + referer);
+////        String currentDomain="";
+////
+////        if (referer == null) {
+////            // 소셜로그인 처음 성공하면 이럼 , 두번째 부터는 null 아님 프론트 주소 가져옴
+////            // TODO: 나중에 수정, 임시로 localhost:3000
+////            referer = "http://localhost:3000/";
+////        }
+////        currentDomain = referer;
+//        currentDomain=referHost;
+//
+//        return currentDomain;
+//    }
 
 
     private void loginSuccess(HttpServletResponse response, CustomOAuth2Member oAuth2Member,String domain) {
